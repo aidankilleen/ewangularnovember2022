84cba8e330b380271daa020bdd07b5fd7a4a675d
Aidan Killeen <aidan.killeen@gmail.com>
2022-11-09 10:19:15 +0000

    fixed modal dialog to behave like bootstrap modal

--- user-edit-investigation/src/app/app.component.ts
+++ user-edit-investigation/src/app/app.component.ts
@@ -9,9 +9,17 @@ import { UserService } from './user.service';
     <button (click)="onShowUserDialog()">Add</button>
 
     <user-dialog 
+      title="Add User"
       *ngIf="showDialog"
       (save)="onSave($event)"
       (close)="showDialog = false">
+    </user-dialog>
+    <user-dialog
+      title="Edit User"
+      *ngIf="showEditDialog"
+      (save)="onUpdate($event)"
+      (close)="showEditDialog = false"
+      [user]="editingUser">
     </user-dialog>
 
     <table>
@@ -29,7 +37,10 @@ import { UserService } from './user.service';
           <td>{{ user.name }}</td>
           <td>{{ user.email }}</td>
           <td>{{ user.active ? "Active" : "Inactive" }}</td>
-          <td><button (click)="onConfirmDeleteUser(user.id)">X</button></td>
+          <td>
+            <button (click)="onConfirmDeleteUser(user.id)">X</button>
+            <button (click)="onShowEditDialog(user)">Edit</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -49,20 +60,30 @@ import { UserService } from './user.service';
 export class AppComponent {
   title = 'user edit investigation';
   showDialog = false;
+  showEditDialog = false;
+  editingUser = new User();
 
   constructor(public userService: UserService) {
+  }
+
+  onShowEditDialog(userToEdit: User) {
+    	this.editingUser = new User(userToEdit.id, userToEdit.name, userToEdit.email, userToEdit.active);
+      this.showEditDialog = true;
   }
 
   onShowUserDialog() {
     this.showDialog = true;
   }
+
   onAddUser() {
     let newUser = new User(99, "new user", "new@gmail.com", false);
     this.userService.addUser(newUser);
   }
+
   onChangeUser(changedUser: User) {
     this.userService.updateUser(changedUser);
   }
+
   onConfirmDeleteUser(id: number) {
     if (confirm("Are you sure?")) {
       this.onDeleteUser(id);
@@ -73,8 +94,14 @@ export class AppComponent {
     //alert(`delete ${id}`);
     this.userService.deleteUser(id);
   }
+
   onSave(newUser: User) {
     this.userService.addUser(newUser);
     this.showDialog = false;
+  }
+
+  onUpdate(userToUpdate: User) {
+    this.userService.updateUser(userToUpdate);
+    this.showEditDialog = false;
   }
 }
--- user-edit-investigation/src/app/user-dialog/user-dialog.component.ts
+++ user-edit-investigation/src/app/user-dialog/user-dialog.component.ts
@@ -5,28 +5,37 @@ import { User } from '../user.model';
 @Component({
   selector: 'user-dialog',
   template: `
-    <div>
-      <h2>Edit</h2>
-      Id: {{ user.id }}<br>
-      Name: <input [(ngModel)]="editingUser.name"><br>
-      Email: <input [(ngModel)]="editingUser.email"><br>
-      Active: <input type="checkbox" [(ngModel)]="editingUser.active">
-      <hr>
-      <button (click)="onSave()">Save</button>
-      <button (click)="onCancel()">Cancel</button>
-    </div>
+    <div class="modal-bg">
+      <div class="modal-content">
+        <h2>{{ title }}</h2>
+        Id: {{ user.id }}<br>
+        Name: <input [(ngModel)]="editingUser.name"><br>
+        Email: <input [(ngModel)]="editingUser.email"><br>
+        Active: <input type="checkbox" [(ngModel)]="editingUser.active">
+        <hr>
+        <button (click)="onSave()">Save</button>
+        <button (click)="onCancel()">Cancel</button>
+      </div>
+  </div>
   `,
   styleUrls: ['./user-dialog.component.css']
 })
 export class UserDialogComponent implements OnInit {
 
+  @Input() title = "User Dialog";
   @Input() user: User = new User();
   @Output() close = new EventEmitter();
   @Output() save = new EventEmitter();
 
-  editingUser = new User(this.user.id, this.user.name, this.user.email, this.user.active);
+  editingUser = new User();
+  
   constructor() { }
   ngOnInit(): void {
+    this.editingUser = new User(
+                          this.user.id, 
+                          this.user.name, 
+                          this.user.email, 
+                          this.user.active);
   }
   onSave() {
     this.save.emit(this.editingUser);
